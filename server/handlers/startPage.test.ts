ea0e262445703b04496e83def6e6bef1d4a32d1e
AlHerbMoj <Alex.Herbert@justice.gov.uk>
2026-06-09 15:34:26 +0100

    Removing redundant comments

--- server/handlers/startPage.test.ts
+++ server/handlers/startPage.test.ts
@@ -2,50 +2,6 @@ import type { NextFunction, Request, Response } from 'express'
 import startPageHandler from './startPage'
 import type { startPageHandlerConfig } from './startPage'
 
-// const mockRes = () => {
-//   const res = {
-//       redirect: jest.fn(),
-//       render: jest.fn(),
-//     }
-//
-//   res.redirect = jest.fn().mockReturnValue(res)
-//   res.render = jest.fn().mockReturnValue(res)
-//
-//   return res
-// }
-//
-// describe('startPageHandler', () => {
-//   it('redirects to external URL in production', () => {
-//     const handler = startPageHandler({
-//       production: true,
-//       productionStartPageUrl: 'https://www.gov.uk/send-prisoner-money',
-//       sendMoneyUrl: 'http://localhost:8004',
-//     })
-//     const res = mockRes()
-//
-//     handler({} as any, res as any, jest.fn())
-//
-//     expect(res.redirect).toHaveBeenCalledWith('https://www.gov.uk/send-prisoner-money')
-//     expect(res.render).not.toHaveBeenCalled()
-//   })
-//
-//   it('displays example start page when not in production', () => {
-//     const handler = startPageHandler({
-//       production: false,
-//       productionStartPageUrl: 'https://www.gov.uk/send-prisoner-money',
-//       sendMoneyUrl: 'http://localhost:8004',
-//     })
-//     const res = mockRes()
-//
-//     handler({} as any, res as any, jest.fn())
-//
-//     expect(res.render).toHaveBeenCalledWith('pages/start-page', { sendMoneyUrl: 'http://localhost:8004' })
-//     expect(res.redirect).not.toHaveBeenCalled()
-//   })
-// })
-
-/* second go */
-
 type ResSubset = Pick<Response, 'redirect' | 'render'>
 
 const makeRes = (): { res: ResSubset; redirect: jest.Mock; render: jest.Mock } => {
