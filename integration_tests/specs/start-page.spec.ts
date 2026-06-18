b7e548e64f3af5b98c2174fdfcf5188a3706ca5b
Harriet-Hall <harriet.hall@justice.gov.uk>
2026-06-18 17:35:22 +0100

    SMTP-87: Moving Start Page to correct URL ("/")

--- integration_tests/specs/start-page.spec.ts
+++ integration_tests/specs/start-page.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('/start-page')
+  await page.goto('/')
 })
 
 test('Heading', async ({ page }) => {
